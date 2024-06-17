a5e3076c9d0231ee4b6ebd5f9b6a3d649ab01b2b
itsmeskylar <albin.spegel@gmail.com>
2024-06-17 14:33:47 +0200

    add ignore for token files

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 import dropbox # type: ignore
 import dropbox.files  # type: ignore
-import discord  # type: ignore
 
-with open("TOKEN.txt", "r") as f:
-    TOKEN = f.read()
+import discord  # type: ignore
+from discord.ext import commands # type: ignore
+import os
+
+with open("TOKEN_DROPBOX.txt", "r") as f:
+    TOKEN_DROPBOX = f.read()
+
+with open("TOKEN_DISCORD.txt", "r") as f:
+    TOKEN_DISCORD = f.read()
 
 # Create a Dropbox object using the access token
-dbx = dropbox.Dropbox(TOKEN)
+dbx = dropbox.Dropbox(TOKEN_DROPBOX)
 
 # Function to list files in a directory
 def list_files(folder_path):
@@ -23,6 +29,13 @@ def list_files(folder_path):
         print(f"API error: {err}")
 
 # Replace with the path of the directory you want to access
-DIRECTORY_PATH = ''
+DIRECTORY_PATH = '/images'
+
+
+
+
+
+
+
 
 list_files(DIRECTORY_PATH)
