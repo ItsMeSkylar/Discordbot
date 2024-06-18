57dbea846dec632c69546fd9d480b3b831943678
itsmeskylar <albin.spegel@gmail.com>
2024-06-18 04:39:51 +0200

    dropbox function into its own script

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from scripts.DropboxScripts import get_or_create_shared_link
+
 import dropbox
 import dropbox.files
 
 import discord
 from discord.ext import commands
 
-with open("TOKEN_DROPBOX.txt", "r") as f:
+with open("tokens/TOKEN_DROPBOX.txt", "r") as f:
     TOKEN_DROPBOX = f.read()
 
-with open("TOKEN_DISCORD.txt", "r") as f:
+with open("tokens/TOKEN_DISCORD.txt", "r") as f:
     TOKEN_DISCORD = f.read()
 
 # Create a Dropbox object using the access token
@@ -21,28 +23,9 @@ client = commands.Bot(command_prefix = '!', intents=intents)
 DIRECTORY_PATH = '/content'
 TEST = "/content/uploads/2024-07/images"
 
-
-def get_or_create_shared_link(path):
-    try:
-        # Try to create a shared link
-        shared_link = dbx.sharing_create_shared_link_with_settings(path)
-        return shared_link.url
-    except dropbox.exceptions.ApiError as err:
-        if isinstance(err.error, dropbox.sharing.CreateSharedLinkWithSettingsError) and err.error.is_shared_link_already_exists():
-            # If the shared link already exists, retrieve the existing link
-            links = dbx.sharing_list_shared_links(path=path)
-            if links.links:
-                return links.links[0].url
-        raise err
-
-
 @client.event
 async def on_ready():
-    print("ready!")
-
-@client.command()
-async def hello(ctx):
-    await ctx.send("test")
+    print("JenniferBot ready!")
 
 @client.command()
 async def folder(ctx):
@@ -52,15 +35,16 @@ async def folder(ctx):
         files = result.entries
         
         # Print out the files
-        for file in files:
-            await ctx.send(f"Name: {file.name}, Type: {'Folder' if isinstance(file, dropbox.files.FolderMetadata) else 'File'}")
+        #for file in files:
+            #await ctx.send(f"Name: {file.name}, Type: {'Folder' if isinstance(file, dropbox.files.FolderMetadata) else 'File'}")
 
         test = files[0]
 
         shared_link_url = get_or_create_shared_link(test.path_lower)
         await ctx.send(f"{shared_link_url}")
+        print(f"posted {test.name} with the following link: {shared_link_url}")
 
-    except dropbox.exceptions.ApiError as err:
+    except Exception as err:
         await ctx.send(f"API error: {err}")
 
 client.run(TOKEN_DISCORD)
